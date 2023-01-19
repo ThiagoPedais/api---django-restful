afb0c429f25d98660a98a6e3ed63804d19f1c59a
Thiago Pedais <luizthiago20sccp@gmail.com>
2023-01-19 12:43:42 -0300

    tests

--- school/courses/urls.py
+++ school/courses/urls.py
@@ -15,7 +15,8 @@ urlpatterns = [
     path('courses/', CoursesAPIView.as_view(), name="courses"),
     path('courses/<int:pk>/', CourseAPIView.as_view(), name="course"),
     path('courses/<int:course_pk>/evaluations/', EvaluationsAPIView.as_view(), name="course_evaluations"),
-    path('courses/<int:course_pk>/evaluations/<int:evaluation_pk>/', EvaluationAPIView.as_view(), name="course_evaluation"),
+    path('courses/<int:course_pk>/evaluations/<int:evaluation_pk>/', EvaluationAPIView.as_view(),
+         name="course_evaluation"),
 
     path('evaluations/', EvaluationsAPIView.as_view(), name="evaluations"),
     path('evaluations/<int:evaluation_pk>/', EvaluationAPIView.as_view(), name="evaluation"),
